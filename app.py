a526b7cdcd281b2c3de4d1c4953c89a00779defe
valixonov04 <163988161+valixonov04@users.noreply.github.com>
2025-05-31 21:28:53 +0500

    push
    AI model

---
+++ app.py
@@ -0,0 +1,31 @@
+#Kerakli kutubxonalar
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+from fasttransform.transform import Transform, Pipeline
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# Sarlavha
+st.title("Bu model sizga mevalarni classifikatsiya qilib beradi..!")
+
+# Rasm yuklash
+file = st.file_uploader("Rasm yuklang", type=["jpg", "jpeg", "png","webp"])
+
+bt = st.button("Raimni tekshirish !!")
+
+if bt:
+    img = PILImage.create(file)
+    # model
+    model = load_learner("model/food_mix.pkl")
+    prediction, _, probs = model.predict(img)
+
+    st.image(img)
+    # Natijani ko‘rsatish
+    st.success(f"Tasnif: **{prediction}** (Ehtimollik: {probs.max().item():.2%})")
+
+    st.toggle("Qayta sinab ko'rish !")
+bt2 = st.button("Sahifani yangilash !")
+if bt2:
+    st.rerun()
